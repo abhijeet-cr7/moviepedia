5cf9c3cbec390456c35e16adf952f1f5631311ef
abhijeet <abhijeetdbz7@gmail.com>
2021-03-31 13:01:50 +0530

    added functionality to unfavourite movie and switching the tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,37 @@
 //  movies : [m1,m2,m3]
 // }
 
-export const ADD_MOVIES = 'ADD_MOVIES';
 
+// action types (for variables)
+export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE';
+export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE';
+export const SET_SHOW_FAVOURITE = 'SET_SHOW_FAVOURITE';
+// action creators (for actions)
 export function addMovies (movies) {
     return {
         type : ADD_MOVIES,
         movies : movies
+    }
+}
+
+export function addFavourite (movie) {
+    return {
+        type : ADD_TO_FAVOURITE,
+        movie : movie
+    }
+}
+
+export function removeFromFavourite(movie) {
+    return {
+        type: REMOVE_FROM_FAVOURITE,
+        movie : movie
+    }
+}
+
+export function setShowFavourite(val) {
+    return {
+        type : SET_SHOW_FAVOURITE,
+        val : val
     }
 }
